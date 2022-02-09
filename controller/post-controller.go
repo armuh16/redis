751aa2f7879be6912be8f797f03e9de6a0f5c481
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-10 02:38:53 +0800

    use redis

--- controller/post-controller.go
+++ controller/post-controller.go
@@ -3,9 +3,11 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
+	"orign/cache"
 	"strings"
 
-	// "gitlab.com/pragmaticreview/golang-mux-api/entity"
+	"gitlab.com/pragmaticreview/golang-mux-api/cache"
+	"gitlab.com/pragmaticreview/golang-mux-api/entity"
 	"gitlab.com/pragmaticreview/golang-mux-api/errors"
 	"gitlab.com/pragmaticreview/golang-mux-api/service"
 )
@@ -14,6 +16,7 @@ type controller struct{}
 
 var (
 	postService service.PostService
+	postCache   cache.PostCache
 )
 
 type PostController interface {
@@ -22,8 +25,9 @@ type PostController interface {
 	AddPost(response http.ResponseWriter, request *http.Request)
 }
 
-func NewPostController(service service.PostService) PostController {
+func NewPostController(service service.PostService, cache cache.PostCache) PostController {
 	postService = service
+	postCache = cache
 	return &controller{}
 }
 
@@ -31,7 +35,7 @@ func (*controller) GetPosts(response http.ResponseWriter, request *http.Request)
 	response.Header().Set("Content-Type", "application/json")
 	posts, err := postService.FindAll()
 	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError{Message: "Error getting the posts"})
+		response.WriteHeader(http.StatusInternalServerError)
 	}
 	response.WriteHeader(http.StatusOK)
 	json.NewEncoder(response).Encode(posts)
@@ -40,12 +44,18 @@ func (*controller) GetPosts(response http.ResponseWriter, request *http.Request)
 func (*controller) GetPostsById(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "application/json")
 	postID := strings.Split(request.URL.Path, "/")[2]
-	posts, err := postService.FindByID(postID)
-	if err != nil {
-		response.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Non posts found!"})
+	var post *entity.Post = postCache.Get(postID)
+	if post == nil {
+		post, err := postService.FindByID(postID)
+		if err != nil {
+			response.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(response).Encode(errors.ServiceError{Message: "Non posts found!"})
+		}
+		postCache.Set(postID, post)
+		response.WriteHeader(http.StatusOK)
+		json.NewEncoder(response).Encode(post)
 	} else {
 		response.WriteHeader(http.StatusOK)
-		json.NewEncoder(response).Encode(posts)
+		json.NewEncoder(response).Encode(post)
 	}
 }
